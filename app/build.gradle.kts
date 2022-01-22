d94a902e78ba6a60d8d01e3764069d87041889c0
Moritz Gstuer <moritz@gstuer.com>
2022-01-22 13:43:11 +0100

    Add java gradle plugin and set target to java 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,13 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+    java
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
 }
 
 repositories {
