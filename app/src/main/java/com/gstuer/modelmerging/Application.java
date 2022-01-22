7556285af1349df49b93fb271b56c73b3885c1c2
Moritz Gstuer <moritz@gstuer.com>
2022-01-22 14:16:31 +0100

    Remove sample content from main class

---
+++ app/src/main/java/com/gstuer/modelmerging/Application.java
@@ -0,0 +1,10 @@
+package com.gstuer.modelmerging;
+
+public class Application {
+    private Application() {
+        throw new IllegalStateException();
+    }
+
+    public static void main(String[] args) {
+    }
+}
