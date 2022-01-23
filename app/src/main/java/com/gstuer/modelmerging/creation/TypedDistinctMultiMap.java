ad906e7a2dde8bde95d1ca9ecbefbcaf04780943
Moritz Gstuer <moritz@gstuer.com>
2022-01-23 18:22:12 +0100

    Add typed map utility classes

---
+++ app/src/main/java/com/gstuer/modelmerging/creation/TypedDistinctMultiMap.java
@@ -0,0 +1,27 @@
+package com.gstuer.modelmerging.creation;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class TypedDistinctMultiMap {
+    private final Map<Class<?>, Set<Object>> data;
+
+    public TypedDistinctMultiMap() {
+        this.data = new HashMap<>();
+    }
+
+    public <T> void put(T element) {
+        Set<Object> elements = this.data.getOrDefault(element.getClass(), new HashSet<>());
+        elements.add(element);
+        this.data.put(element.getClass(), elements);
+    }
+
+    public <T> List<T> get(Class<T> key) {
+        Set<Object> objects = this.data.getOrDefault(key, new HashSet<>());
+        return objects.stream().map(key::cast).collect(Collectors.toList());
+    }
+}
---
+++ app/src/main/java/com/gstuer/modelmerging/creation/TypedMap.java
@@ -0,0 +1,20 @@
+package com.gstuer.modelmerging.creation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TypedMap {
+    private final Map<Class<?>, Object> data;
+
+    public TypedMap() {
+        this.data = new HashMap<>();
+    }
+
+    public <T> void put(T element) {
+        this.data.put(element.getClass(), element);
+    }
+
+    public <T> T get(Class<T> key) {
+        return key.cast(this.data.get(key));
+    }
+}
