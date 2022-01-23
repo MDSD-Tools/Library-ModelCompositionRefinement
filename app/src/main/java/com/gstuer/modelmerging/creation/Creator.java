6757dcb689641d4aeffde1ecdef407e3b77ef82e
Moritz Gstuer <moritz@gstuer.com>
2022-01-23 18:25:05 +0100

    Add draft of model creator abstract class

---
+++ app/src/main/java/com/gstuer/modelmerging/creation/Creator.java
@@ -0,0 +1,37 @@
+package com.gstuer.modelmerging.creation;
+
+import com.gstuer.modelmerging.discovery.Discoverer;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public abstract class Creator<M extends Model> {
+    private static final String UNSUPPORTED_DISCOVERER = "No merger available for discoverer of type %s.";
+
+    private M model;
+
+    protected Creator(M model) {
+        this.model = Objects.requireNonNull(model);
+    }
+
+    public M create(Discoverer<?>... discoverers) {
+        for (Discoverer<?> discoverer : discoverers) {
+            mergeDiscoverer(discoverer);
+        }
+        return this.model;
+    }
+
+    protected abstract <T> Optional<Merger<M, T>> getMergerForDiscoveryType(Class<T> discoveryType);
+
+    private <K> void mergeDiscoverer(Discoverer<K> discoverer) {
+        Optional<Merger<M, K>> optionalMerger = getMergerForDiscoveryType(discoverer.getDiscoveryType());
+        if (optionalMerger.isPresent()) {
+            Merger<M, K> merger = optionalMerger.get();
+            for (K discovery : discoverer.getDiscoveries()) {
+                this.model = merger.merge(this.model, discovery);
+            }
+        } else {
+            throw new UnsupportedDiscovererException(UNSUPPORTED_DISCOVERER.formatted(discoverer.getClass()));
+        }
+    }
+}
