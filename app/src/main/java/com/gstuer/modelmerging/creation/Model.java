90d3f99aa63e282218c0cb1e9674eaade16f45a4
Moritz Gstuer <moritz@gstuer.com>
2022-01-23 18:22:50 +0100

    Add first draft of model interface

---
+++ app/src/main/java/com/gstuer/modelmerging/creation/Model.java
@@ -0,0 +1,13 @@
+package com.gstuer.modelmerging.creation;
+
+import java.util.List;
+
+public interface Model {
+    <T> List<T> getElementsByType(Class<T> elementType);
+
+    <T> void addElement(T element);
+
+    <T> List<T> getRelationsByType(Class<T> relationType);
+
+    <T> void addRelation(T relation);
+}
