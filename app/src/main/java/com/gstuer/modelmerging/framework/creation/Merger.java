bb166f077286f694a22e158b7d0f7cf41b13f05e
Moritz Gstuer <moritz@gstuer.com>
2022-05-17 17:52:09 +0200

    Make merger stateful & add implications to merger

--- app/src/main/java/com/gstuer/modelmerging/framework/creation/Merger.java
+++ app/src/main/java/com/gstuer/modelmerging/framework/creation/Merger.java
@@ -1,7 +1,28 @@
 package com.gstuer.modelmerging.framework.creation;
 
 import com.gstuer.modelmerging.framework.surrogate.Model;
+import com.gstuer.modelmerging.framework.surrogate.Replaceable;
 
-public interface Merger<M extends Model, T> {
-    M merge(M model, T discovery);
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public abstract class Merger<M extends Model, T extends Replaceable> {
+    private final M model;
+    private final Set<Replaceable> implications;
+
+    protected Merger(M model) {
+        this.model = Objects.requireNonNull(model);
+        this.implications = new HashSet<>();
+    }
+
+    public abstract void merge(T discovery);
+
+    public M getModel() {
+        return model;
+    }
+
+    public Set<Replaceable> getImplications() {
+        return Set.copyOf(implications);
+    }
 }
