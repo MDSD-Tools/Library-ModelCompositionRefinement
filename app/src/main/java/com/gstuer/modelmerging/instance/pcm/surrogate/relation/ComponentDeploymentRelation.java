6a10a2ebb4117c009647d9bf1ffe49b343dbaec6
Moritz Gstuer <moritz@gstuer.com>
2022-05-21 18:21:56 +0200

    Fix wrong generic type of comp. depl. relation

--- app/src/main/java/com/gstuer/modelmerging/instance/pcm/surrogate/relation/ComponentDeploymentRelation.java
+++ app/src/main/java/com/gstuer/modelmerging/instance/pcm/surrogate/relation/ComponentDeploymentRelation.java
@@ -2,9 +2,10 @@ package com.gstuer.modelmerging.instance.pcm.surrogate.relation;
 
 import com.gstuer.modelmerging.framework.surrogate.Relation;
 import com.gstuer.modelmerging.instance.pcm.surrogate.element.Component;
+import com.gstuer.modelmerging.instance.pcm.surrogate.element.Deployment;
 
-public class ComponentDeploymentRelation extends Relation<Component, Component> {
-    public ComponentDeploymentRelation(Component source, Component destination, boolean isPlaceholder) {
+public class ComponentDeploymentRelation extends Relation<Component, Deployment> {
+    public ComponentDeploymentRelation(Component source, Deployment destination, boolean isPlaceholder) {
         super(source, destination, isPlaceholder);
     }
 }
