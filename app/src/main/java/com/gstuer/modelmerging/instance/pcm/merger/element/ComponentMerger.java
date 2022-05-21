49d6eeb4a6765d9dcf3672f9e464e83e27d30ba8
Moritz Gstuer <moritz@gstuer.com>
2022-05-21 18:23:02 +0200

    Implement refinement for component element merge

--- app/src/main/java/com/gstuer/modelmerging/instance/pcm/merger/element/ComponentMerger.java
+++ app/src/main/java/com/gstuer/modelmerging/instance/pcm/merger/element/ComponentMerger.java
@@ -3,6 +3,11 @@ package com.gstuer.modelmerging.instance.pcm.merger.element;
 import com.gstuer.modelmerging.framework.merger.Merger;
 import com.gstuer.modelmerging.instance.pcm.surrogate.PcmSurrogate;
 import com.gstuer.modelmerging.instance.pcm.surrogate.element.Component;
+import com.gstuer.modelmerging.instance.pcm.surrogate.element.Deployment;
+import com.gstuer.modelmerging.instance.pcm.surrogate.relation.ComponentDeploymentRelation;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ComponentMerger extends Merger<PcmSurrogate, Component> {
     public ComponentMerger(PcmSurrogate model) {
@@ -11,6 +16,15 @@ public class ComponentMerger extends Merger<PcmSurrogate, Component> {
 
     @Override
     protected void refine(Component discovery) {
-        // TODO Add refinement steps
+        List<ComponentDeploymentRelation> deploymentRelations = getModel().getByType(ComponentDeploymentRelation.class)
+                .stream()
+                .dropWhile(relation -> !relation.getSource().equals(discovery))
+                .collect(Collectors.toList());
+        if (deploymentRelations.isEmpty()) {
+            // TODO Replace with concrete pcm placeholder deployment
+            Deployment deployment = new Deployment("Placeholder", true);
+            ComponentDeploymentRelation relation = new ComponentDeploymentRelation(discovery, deployment, true);
+            addImplication(relation);
+        }
     }
 }
