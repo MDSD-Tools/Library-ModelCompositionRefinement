a4e786d792980d18e48c168031f402c8c64591e0
Moritz Gstuer <moritz@gstuer.com>
2022-05-21 15:24:47 +0200

    Remove deprecated exception constructor

--- app/src/main/java/com/gstuer/modelmerging/framework/orchestration/UnavailableMergerException.java
+++ app/src/main/java/com/gstuer/modelmerging/framework/orchestration/UnavailableMergerException.java
@@ -4,11 +4,6 @@ public class UnavailableMergerException extends IllegalArgumentException {
     private static final long serialVersionUID = 7358182453030322135L;
     private static final String MESSAGE_PATTERN = "No merger available for discovery of type %s.";
 
-    @Deprecated
-    public UnavailableMergerException(String message) {
-        super(message);
-    }
-
     public UnavailableMergerException(Class<?> discoveryType) {
         super(String.format(MESSAGE_PATTERN, discoveryType.getName()));
     }
