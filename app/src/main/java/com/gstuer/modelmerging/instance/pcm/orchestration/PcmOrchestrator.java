f96461941d43b99d1e94523b900e827b6780051d
Moritz Gstuer <moritz@gstuer.com>
2022-05-21 18:17:23 +0200

    Fix dupl. merger in pcm orchestrator constructor

--- app/src/main/java/com/gstuer/modelmerging/instance/pcm/orchestration/PcmOrchestrator.java
+++ app/src/main/java/com/gstuer/modelmerging/instance/pcm/orchestration/PcmOrchestrator.java
@@ -4,12 +4,13 @@ import com.gstuer.modelmerging.framework.orchestration.Orchestrator;
 import com.gstuer.modelmerging.instance.pcm.merger.element.ComponentMerger;
 import com.gstuer.modelmerging.instance.pcm.merger.element.DeploymentMerger;
 import com.gstuer.modelmerging.instance.pcm.merger.relation.ComponentComponentRelationMerger;
+import com.gstuer.modelmerging.instance.pcm.merger.relation.ComponentDeploymentRelationMerger;
 import com.gstuer.modelmerging.instance.pcm.surrogate.PcmSurrogate;
 
 public class PcmOrchestrator extends Orchestrator<PcmSurrogate> {
     public PcmOrchestrator(PcmSurrogate model) {
         super(model, new ComponentMerger(model), new DeploymentMerger(model),
-                new ComponentComponentRelationMerger(model), new DeploymentMerger(model));
+                new ComponentComponentRelationMerger(model), new ComponentDeploymentRelationMerger(model));
     }
 
     public PcmOrchestrator() {
