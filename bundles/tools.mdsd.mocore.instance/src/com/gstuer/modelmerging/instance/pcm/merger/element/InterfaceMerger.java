f36777721358a2bdc6d51bc762b43b03857792ca
Moritz Gstuer <moritz@gstuer.com>
2022-06-14 16:51:21 +0200

    Add merger for interface elements

---
+++ bundles/tools.mdsd.mocore.instance/src/com/gstuer/modelmerging/instance/pcm/merger/element/InterfaceMerger.java
@@ -0,0 +1,31 @@
+package com.gstuer.modelmerging.instance.pcm.merger.element;
+
+import java.util.List;
+
+import com.gstuer.modelmerging.framework.merger.Merger;
+import com.gstuer.modelmerging.instance.pcm.surrogate.PcmSurrogate;
+import com.gstuer.modelmerging.instance.pcm.surrogate.element.Component;
+import com.gstuer.modelmerging.instance.pcm.surrogate.element.Interface;
+import com.gstuer.modelmerging.instance.pcm.surrogate.relation.ProvidesRelation;
+
+public class InterfaceMerger extends Merger<PcmSurrogate, Interface> {
+    public InterfaceMerger(PcmSurrogate model) {
+        super(model, Interface.class);
+    }
+
+    @Override
+    protected void refine(Interface discovery) {
+        List<ProvidesRelation> providesRelations = getModel().getByType(ProvidesRelation.class);
+        providesRelations.removeIf(relation -> !relation.getDestination().equals(discovery));
+        List<ProvidesRelation> requiresRelations = getModel().getByType(ProvidesRelation.class);
+        requiresRelations.removeIf(relation -> !relation.getDestination().equals(discovery));
+
+        if (providesRelations.isEmpty() && requiresRelations.isEmpty()) {
+            // TODO Replace with concrete pcm placeholder
+            Component component = new Component("Placeholder", true);
+            ProvidesRelation relation = new ProvidesRelation(component, discovery, true);
+            addImplication(relation);
+        }
+    }
+
+}
