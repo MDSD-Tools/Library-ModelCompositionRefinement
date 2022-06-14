49af08b4574bc02b0829956611ea7e0836e663c0
Moritz Gstuer <moritz@gstuer.com>
2022-06-14 14:45:49 +0200

    Add instance elements for signature & interface

---
+++ bundles/tools.mdsd.mocore.instance/src/com/gstuer/modelmerging/instance/pcm/surrogate/element/Interface.java
@@ -0,0 +1,10 @@
+package com.gstuer.modelmerging.instance.pcm.surrogate.element;
+
+import com.gstuer.modelmerging.framework.surrogate.Element;
+
+// TODO Change generic to real palladio component
+public class Interface extends Element<String> {
+    public Interface(String value, boolean isPlaceholder) {
+        super(value, isPlaceholder);
+    }
+}
---
+++ bundles/tools.mdsd.mocore.instance/src/com/gstuer/modelmerging/instance/pcm/surrogate/element/Signature.java
@@ -0,0 +1,10 @@
+package com.gstuer.modelmerging.instance.pcm.surrogate.element;
+
+import com.gstuer.modelmerging.framework.surrogate.Element;
+
+// TODO Change generic to real palladio component
+public class Signature extends Element<String> {
+    public Signature(String value, boolean isPlaceholder) {
+        super(value, isPlaceholder);
+    }
+}
