1205d51c7a13c0b7b93f46af568ccf76cfc77caa
Moritz Gstuer <moritz@gstuer.com>
2022-06-15 19:33:32 +0200

    Simplify src/dest replacement for sub relations

--- bundles/tools.mdsd.mocore.framework/src/com/gstuer/modelmerging/framework/surrogate/Relation.java
+++ bundles/tools.mdsd.mocore.framework/src/com/gstuer/modelmerging/framework/surrogate/Relation.java
@@ -25,10 +25,32 @@ public abstract class Relation<T extends Replaceable, S extends Replaceable> ext
         return this.source.canReplace(replaceable) || this.destination.canReplace(replaceable);
     }
 
+    @SuppressWarnings("unchecked")
+    protected <U extends Replaceable> T getSourceReplacement(U original, U replacement) {
+        if (this.source.canReplace(original)) {
+            // Type-safe cast due to possible replacement of source with original type.
+            return (T) replacement;
+        }
+        return this.source;
+    }
+
+    @SuppressWarnings("unchecked")
+    protected <U extends Replaceable> S getDestinationReplacement(U original, U replacement) {
+        if (this.destination.canReplace(original)) {
+            // Type-safe cast due to possible replacement of source with original type.
+            return (S) replacement;
+        }
+        return this.destination;
+    }
+
     @Override
     public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
         Relation<?, ?> relation = (Relation<?, ?>) object;
         return source.equals(relation.source) && destination.equals(relation.destination);
     }
