9361d21034b73a343bc81a32e24bce4faa437084
Moritz Gstuer <moritz@gstuer.com>
2022-06-15 18:39:29 +0200

    Fix replace in model falsely adding successors
    The model does not add successors to the model anymore but returns them
    as implications of the replacement and removes the predecessors from the
    model. This change is needed to be able to traverse affected relations.

--- bundles/tools.mdsd.mocore.framework/src/com/gstuer/modelmerging/framework/surrogate/Model.java
+++ bundles/tools.mdsd.mocore.framework/src/com/gstuer/modelmerging/framework/surrogate/Model.java
@@ -1,6 +1,7 @@
 package com.gstuer.modelmerging.framework.surrogate;
 
 import java.util.List;
+import java.util.Set;
 
 public interface Model {
     <T extends Replaceable> List<T> getByType(Class<T> type);
@@ -8,6 +9,19 @@ public interface Model {
     void add(Replaceable replaceable);
 
     boolean contains(Replaceable replaceable);
-    
-    void replace(Replaceable original, Replaceable replacement);
+
+    /**
+     * Executes a replacement of a certain {@link Replaceable replaceable} in the
+     * model. For this purpose, all affected replaceables are removed from the
+     * model. The appropriate successors of the removed replaceables are created and
+     * returned but not added to the model. The given original replaceable to be
+     * replaced is also removed from the model. The given replacement is part of the
+     * returned successors but not added to the model.
+     *
+     * @param original    the replaceable to be replaced in the model
+     * @param replacement the successor of the replaceable to be replaced
+     * @return The successors of the affected and removed replaceables in the model
+     *         including the original's replacement.
+     */
+    Set<Replaceable> replace(Replaceable original, Replaceable replacement);
 }
--- bundles/tools.mdsd.mocore.instance/src/com/gstuer/modelmerging/instance/pcm/surrogate/PcmSurrogate.java
+++ bundles/tools.mdsd.mocore.instance/src/com/gstuer/modelmerging/instance/pcm/surrogate/PcmSurrogate.java
@@ -1,6 +1,8 @@
 package com.gstuer.modelmerging.instance.pcm.surrogate;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.gstuer.modelmerging.framework.surrogate.Model;
 import com.gstuer.modelmerging.framework.surrogate.Replaceable;
@@ -29,13 +31,21 @@ public class PcmSurrogate implements Model {
     }
 
     @Override
-    public void replace(Replaceable original, Replaceable replacement) {
+    public Set<Replaceable> replace(Replaceable original, Replaceable replacement) {
+        Set<Replaceable> implications = new HashSet<>();
         for (Replaceable predecessor : this.replaceables) {
             if (predecessor.canReplace(original)) {
+                /*
+                 * If a predecessor needs replacement, create successor of obsolete replaceable.
+                 * Then delete obsolete replaceable from model and return successor as
+                 * implication for further merging. The model cannot add the successor itself
+                 * because it might have consequences for other replaceables in the model.
+                 */
                 Replaceable successor = predecessor.replace(original, replacement);
                 this.replaceables.remove(predecessor);
-                this.add(successor);
+                implications.add(successor);
             }
         }
+        return implications;
     }
 }
