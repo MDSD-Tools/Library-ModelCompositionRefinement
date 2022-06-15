42b8381e976ef192e4587645c5cde9d50c970e7e
Moritz Gstuer <moritz@gstuer.com>
2022-06-15 17:57:49 +0200

    Add replacement mechanism for elements

--- bundles/tools.mdsd.mocore.framework/src/com/gstuer/modelmerging/framework/surrogate/Element.java
+++ bundles/tools.mdsd.mocore.framework/src/com/gstuer/modelmerging/framework/surrogate/Element.java
@@ -13,12 +13,20 @@ public abstract class Element<T> extends Replaceable {
     public T getValue() {
         return value;
     }
-    
+
     @Override
     public boolean canReplace(Replaceable replaceable) {
-        return false;
+        return this.equals(replaceable);
     }
-    
+
+    @Override
+    public <U extends Replaceable> U replace(U original, U replacement) {
+        if (this.canReplace(original)) {
+            throw new IllegalArgumentException();
+        }
+        return replacement;
+    }
+
     @Override
     public boolean equals(Object object) {
         if (this == object) return true;
