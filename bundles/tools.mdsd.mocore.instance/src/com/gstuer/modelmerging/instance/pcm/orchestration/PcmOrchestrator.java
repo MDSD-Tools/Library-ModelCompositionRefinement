c731199e99b523a56f2ae04cbc6e4dab093d6545
Moritz Gstuer <moritz@gstuer.com>
2022-06-17 15:46:17 +0200

    Add note for missing mergers in orchestrator

--- bundles/tools.mdsd.mocore.instance/src/com/gstuer/modelmerging/instance/pcm/orchestration/PcmOrchestrator.java
+++ bundles/tools.mdsd.mocore.instance/src/com/gstuer/modelmerging/instance/pcm/orchestration/PcmOrchestrator.java
@@ -3,12 +3,13 @@ package com.gstuer.modelmerging.instance.pcm.orchestration;
 import com.gstuer.modelmerging.framework.orchestration.Orchestrator;
 import com.gstuer.modelmerging.instance.pcm.merger.element.ComponentMerger;
 import com.gstuer.modelmerging.instance.pcm.merger.element.DeploymentMerger;
-import com.gstuer.modelmerging.instance.pcm.merger.relation.ComponentAssemblyRelationMerger;
 import com.gstuer.modelmerging.instance.pcm.merger.relation.ComponentAllocationRelationMerger;
+import com.gstuer.modelmerging.instance.pcm.merger.relation.ComponentAssemblyRelationMerger;
 import com.gstuer.modelmerging.instance.pcm.surrogate.PcmSurrogate;
 
 public class PcmOrchestrator extends Orchestrator<PcmSurrogate> {
     public PcmOrchestrator(PcmSurrogate model) {
+        // TODO Add missing mergers to constructor
         super(model, new ComponentMerger(model), new DeploymentMerger(model),
                 new ComponentAssemblyRelationMerger(model), new ComponentAllocationRelationMerger(model));
     }
