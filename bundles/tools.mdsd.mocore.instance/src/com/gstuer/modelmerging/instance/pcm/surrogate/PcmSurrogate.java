7f3d8f2cf8b9e27870e5574cbea7e0e370689594
Moritz Gstuer <moritz@gstuer.com>
2022-06-18 22:12:53 +0200

    Add missing guard for null in PcmSurrogate.replace

--- bundles/tools.mdsd.mocore.instance/src/com/gstuer/modelmerging/instance/pcm/surrogate/PcmSurrogate.java
+++ bundles/tools.mdsd.mocore.instance/src/com/gstuer/modelmerging/instance/pcm/surrogate/PcmSurrogate.java
@@ -2,6 +2,7 @@ package com.gstuer.modelmerging.instance.pcm.surrogate;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 import com.gstuer.modelmerging.framework.surrogate.Model;
@@ -32,14 +33,16 @@ public class PcmSurrogate implements Model {
 
     @Override
     public Set<Replaceable> replace(Replaceable original, Replaceable replacement) {
+        if (Objects.isNull(original) || Objects.isNull(replacement)) {
+            throw new NullPointerException();
+        }
         Set<Replaceable> implications = new HashSet<>();
         for (Replaceable predecessor : this.replaceables) {
             if (predecessor.canReplace(original)) {
                 /*
-                 * If a predecessor needs replacement, create successor of obsolete replaceable.
-                 * Then delete obsolete replaceable from model and return successor as
-                 * implication for further merging. The model cannot add the successor itself
-                 * because it might have consequences for other replaceables in the model.
+                 * If a predecessor needs replacement, create successor of obsolete replaceable. Then delete obsolete
+                 * replaceable from model and return successor as implication for further merging. The model cannot add
+                 * the successor itself because it might have consequences for other replaceables in the model.
                  */
                 Replaceable successor = predecessor.replace(original, replacement);
                 this.replaceables.remove(predecessor);
