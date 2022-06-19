12d7886c3bc4ee2b05138e8b28e493a8e79b5f3e
Moritz Gstuer <moritz@gstuer.com>
2022-06-19 16:56:04 +0200

    Add test for refine implications of deployments

--- tests/tools.mdsd.mocore.test/src/com/gstuer/modelmerging/instance/pcm/merger/DeploymentMergerTest.java
+++ tests/tools.mdsd.mocore.test/src/com/gstuer/modelmerging/instance/pcm/merger/DeploymentMergerTest.java
@@ -1,11 +1,32 @@
 package com.gstuer.modelmerging.instance.pcm.merger;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 import com.gstuer.modelmerging.framework.merger.MergerTest;
 import com.gstuer.modelmerging.instance.pcm.merger.element.DeploymentMerger;
 import com.gstuer.modelmerging.instance.pcm.surrogate.PcmSurrogate;
 import com.gstuer.modelmerging.instance.pcm.surrogate.element.Deployment;
 
 public class DeploymentMergerTest extends MergerTest<DeploymentMerger, PcmSurrogate, Deployment> {
+    @Test
+    public void testRefineWithValidElementAddsCorrectImplications() {
+        // Test data
+        PcmSurrogate model = createEmptyModel();
+        DeploymentMerger merger = createMerger(model);
+        Deployment element = createUniqueReplaceable();
+
+        // Assertions: Pre-execution
+        assertTrue(merger.getImplications().isEmpty());
+
+        // Execution
+        merger.refine(element);
+
+        // Assertions: Post-execution
+        assertTrue(merger.getImplications().isEmpty());
+    }
+
     @Override
     protected DeploymentMerger createMerger(PcmSurrogate model) {
         return new DeploymentMerger(model);
