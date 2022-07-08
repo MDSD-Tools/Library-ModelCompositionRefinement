865dc91d48ac3f225be3e05df7f2ee906a7e4529
Moritz Gstuer <moritz@gstuer.com>
2022-07-08 12:24:03 +0200

    Adapt pcm orchestrator test to new element types

--- tests/tools.mdsd.mocore.test/src/com/gstuer/modelmerging/instance/pcm/orchestration/PcmOrchestratorTest.java
+++ tests/tools.mdsd.mocore.test/src/com/gstuer/modelmerging/instance/pcm/orchestration/PcmOrchestratorTest.java
@@ -1,22 +1,24 @@
 package com.gstuer.modelmerging.instance.pcm.orchestration;
 
-import com.gstuer.modelmerging.instance.pcm.surrogate.PcmSurrogate;
-import com.gstuer.modelmerging.instance.pcm.surrogate.element.Component;
-import com.gstuer.modelmerging.instance.pcm.surrogate.element.Deployment;
-import com.gstuer.modelmerging.instance.pcm.surrogate.relation.ComponentAllocationRelation;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+import com.gstuer.modelmerging.instance.pcm.surrogate.PcmSurrogate;
+import com.gstuer.modelmerging.instance.pcm.surrogate.element.Component;
+import com.gstuer.modelmerging.instance.pcm.surrogate.element.Deployment;
+import com.gstuer.modelmerging.instance.pcm.surrogate.relation.ComponentAllocationRelation;
+import com.gstuer.modelmerging.test.utility.ElementFactory;
 
 public class PcmOrchestratorTest {
     @Test
     public void testExistsComponentAfterProcess() {
         PcmOrchestrator orchestrator = new PcmOrchestrator();
-        Component component = new Component("TestComponent", false);
+        Component component = ElementFactory.createUniqueComponent(false);
         orchestrator.processDiscovery(component);
         assertTrue(orchestrator.getModel().contains(component));
     }
@@ -24,12 +26,13 @@ public class PcmOrchestratorTest {
     @Test
     public void testExistImplicitReplaceablesAfterProcess() {
         PcmOrchestrator orchestrator = new PcmOrchestrator();
-        Component component = new Component("TestComponent", false);
+        Component component = ElementFactory.createUniqueComponent(false);
         orchestrator.processDiscovery(component);
 
         PcmSurrogate model = orchestrator.getModel();
         List<Deployment> deployments = model.getByType(Deployment.class);
-        Stream<ComponentAllocationRelation> componentDeploymentRelations = model.getByType(ComponentAllocationRelation.class).stream();
+        Stream<ComponentAllocationRelation> componentDeploymentRelations = model
+                .getByType(ComponentAllocationRelation.class).stream();
 
         // Assertions
         assertTrue(model.contains(component));
@@ -40,7 +43,7 @@ public class PcmOrchestratorTest {
     @Test
     public void testExistsDeploymentAfterProcess() {
         PcmOrchestrator orchestrator = new PcmOrchestrator();
-        Deployment deployment = new Deployment("TestDeployment", false);
+        Deployment deployment = ElementFactory.createUniqueDeployment(false);
         orchestrator.processDiscovery(deployment);
         assertTrue(orchestrator.getModel().contains(deployment));
     }
