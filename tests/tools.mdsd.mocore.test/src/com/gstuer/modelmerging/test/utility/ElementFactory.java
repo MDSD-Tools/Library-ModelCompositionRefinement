6079f7a3c22ff069f983d2d5ee4048b2136ebcf5
Moritz Gstuer <moritz@gstuer.com>
2022-07-08 12:20:14 +0200

    Add element factory test utility for pcm wrappers

---
+++ tests/tools.mdsd.mocore.test/src/com/gstuer/modelmerging/test/utility/ElementFactory.java
@@ -0,0 +1,46 @@
+package com.gstuer.modelmerging.test.utility;
+
+import org.palladiosimulator.generator.fluent.repository.factory.FluentRepositoryFactory;
+import org.palladiosimulator.pcm.repository.BasicComponent;
+import org.palladiosimulator.pcm.repository.OperationInterface;
+import org.palladiosimulator.pcm.repository.OperationSignature;
+import org.palladiosimulator.pcm.repository.RepositoryFactory;
+import org.palladiosimulator.pcm.resourceenvironment.ResourceContainer;
+import org.palladiosimulator.pcm.resourceenvironment.ResourceenvironmentFactory;
+
+import com.gstuer.modelmerging.instance.pcm.surrogate.element.Component;
+import com.gstuer.modelmerging.instance.pcm.surrogate.element.Deployment;
+import com.gstuer.modelmerging.instance.pcm.surrogate.element.Interface;
+import com.gstuer.modelmerging.instance.pcm.surrogate.element.Signature;
+
+public final class ElementFactory {
+    private ElementFactory() {
+
+    }
+
+    public static Signature createUniqueSignature(boolean isPlaceholder) {
+        String identifier = IdentifierGenerator.getUniqueIdentifier();
+        OperationSignature value = RepositoryFactory.eINSTANCE.createOperationSignature();
+        value.setEntityName(identifier);
+        return new Signature(value, isPlaceholder);
+    }
+
+    public static Interface createUniqueInterface(boolean isPlaceholder) {
+        String identifier = IdentifierGenerator.getUniqueIdentifier();
+        OperationInterface value = new FluentRepositoryFactory().newOperationInterface().withName(identifier).build();
+        return new Interface(value, isPlaceholder);
+    }
+
+    public static Component createUniqueComponent(boolean isPlaceholder) {
+        String identifier = IdentifierGenerator.getUniqueIdentifier();
+        BasicComponent value = new FluentRepositoryFactory().newBasicComponent().withName(identifier).build();
+        return new Component(value, isPlaceholder);
+    }
+
+    public static Deployment createUniqueDeployment(boolean isPlaceholder) {
+        String identifier = IdentifierGenerator.getUniqueIdentifier();
+        ResourceContainer value = ResourceenvironmentFactory.eINSTANCE.createResourceContainer();
+        value.setEntityName(identifier);
+        return new Deployment(value, isPlaceholder);
+    }
+}
