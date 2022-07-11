87eeb5160913250d12db4f2804f52ab488eff7d9
Moritz Gstuer <moritz@gstuer.com>
2022-07-11 11:48:00 +0200

    Add implementation for allocation transformer

--- bundles/tools.mdsd.mocore.instance/src/com/gstuer/modelmerging/instance/pcm/transformation/AllocationTransformer.java
+++ bundles/tools.mdsd.mocore.instance/src/com/gstuer/modelmerging/instance/pcm/transformation/AllocationTransformer.java
@@ -1,13 +1,17 @@
 package com.gstuer.modelmerging.instance.pcm.transformation;
 
+import java.util.List;
+
 import org.palladiosimulator.generator.fluent.allocation.api.IAllocationAddition;
 import org.palladiosimulator.generator.fluent.allocation.factory.FluentAllocationFactory;
+import org.palladiosimulator.generator.fluent.allocation.structure.AllocationContextCreator;
 import org.palladiosimulator.pcm.allocation.Allocation;
 import org.palladiosimulator.pcm.resourceenvironment.ResourceEnvironment;
 import org.palladiosimulator.pcm.system.System;
 
 import com.gstuer.modelmerging.framework.transformation.Transformer;
 import com.gstuer.modelmerging.instance.pcm.surrogate.PcmSurrogate;
+import com.gstuer.modelmerging.instance.pcm.surrogate.relation.ComponentAllocationRelation;
 
 public class AllocationTransformer implements Transformer<PcmSurrogate, Allocation> {
     @Override
@@ -19,8 +23,26 @@ public class AllocationTransformer implements Transformer<PcmSurrogate, Allocati
                 .withSystem(system)
                 .withResourceEnvironment(resourceEnvironment);
 
-        // TODO Add assembly context (Component) <=> resource container (Deployment) relations
+        // Add allocation contexts to allocation
+        List<ComponentAllocationRelation> relations = model.getByType(ComponentAllocationRelation.class);
+        for (ComponentAllocationRelation relation : relations) {
+            // Get and add context (creator) for specific allocation relation
+            AllocationContextCreator contextCreator = getCreator(relation);
+            fluentAllocation.addToAllocation(contextCreator);
+        }
 
         return fluentAllocation.createAllocationNow();
+    }
+
+    private AllocationContextCreator getCreator(ComponentAllocationRelation relation) {
+        AllocationContextCreator contextCreator = new FluentAllocationFactory().newAllocationContext();
+
+        // Use name of entities to fetch up-to-date entities from system and resource environment
+        String componentEntityName = relation.getSource().getValue().getEntityName();
+        String deploymentEntityName = relation.getDestination().getValue().getEntityName();
+        // Assembly context represents component in system view
+        contextCreator.withAssemblyContext(componentEntityName).withResourceContainer(deploymentEntityName);
+
+        return contextCreator;
     }
 }
