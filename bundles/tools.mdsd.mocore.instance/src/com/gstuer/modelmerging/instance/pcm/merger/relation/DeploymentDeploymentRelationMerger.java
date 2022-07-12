c4fd92602369b9d667024282dad9d7f5571ea73c
Moritz Gstuer <moritz@gstuer.com>
2022-07-12 16:11:22 +0200

    Add & test specification added to depl->depl

--- bundles/tools.mdsd.mocore.instance/src/com/gstuer/modelmerging/instance/pcm/merger/relation/DeploymentDeploymentRelationMerger.java
+++ bundles/tools.mdsd.mocore.instance/src/com/gstuer/modelmerging/instance/pcm/merger/relation/DeploymentDeploymentRelationMerger.java
@@ -1,11 +1,33 @@
 package com.gstuer.modelmerging.instance.pcm.merger.relation;
 
+import java.util.List;
+import java.util.Objects;
+
 import com.gstuer.modelmerging.framework.merger.RelationMerger;
 import com.gstuer.modelmerging.instance.pcm.surrogate.PcmSurrogate;
+import com.gstuer.modelmerging.instance.pcm.surrogate.element.LinkResourceSpecification;
 import com.gstuer.modelmerging.instance.pcm.surrogate.relation.DeploymentDeploymentRelation;
+import com.gstuer.modelmerging.instance.pcm.surrogate.relation.LinkResourceSpecificationRelation;
 
 public class DeploymentDeploymentRelationMerger extends RelationMerger<PcmSurrogate, DeploymentDeploymentRelation> {
     public DeploymentDeploymentRelationMerger(PcmSurrogate model) {
         super(model, DeploymentDeploymentRelation.class);
+    }
+
+    @Override
+    protected void refine(DeploymentDeploymentRelation discovery) {
+        super.refine(discovery);
+
+        // Check whether a linking resource specification already exists for the link to be merged
+        List<LinkResourceSpecificationRelation> specifications = this.getModel()
+                .getByType(LinkResourceSpecificationRelation.class);
+        specifications.removeIf(specification -> !Objects.equals(discovery, specification.getDestination()));
+        if (specifications.isEmpty()) {
+            // Add implicit placeholder specification, if no specification is found for this deployment link
+            LinkResourceSpecification placeholderSpecification = LinkResourceSpecification.getUniquePlaceholder();
+            LinkResourceSpecificationRelation implicitRelation = new LinkResourceSpecificationRelation(
+                    placeholderSpecification, discovery, true);
+            this.addImplication(implicitRelation);
+        }
     }
 }
--- tests/tools.mdsd.mocore.test/src/com/gstuer/modelmerging/instance/pcm/merger/relation/DeploymentDeploymentRelationMergerTest.java
+++ tests/tools.mdsd.mocore.test/src/com/gstuer/modelmerging/instance/pcm/merger/relation/DeploymentDeploymentRelationMergerTest.java
@@ -1,13 +1,46 @@
 package com.gstuer.modelmerging.instance.pcm.merger.relation;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
 import com.gstuer.modelmerging.framework.merger.RelationMergerTest;
+import com.gstuer.modelmerging.framework.surrogate.Replaceable;
 import com.gstuer.modelmerging.instance.pcm.surrogate.PcmSurrogate;
 import com.gstuer.modelmerging.instance.pcm.surrogate.element.Deployment;
 import com.gstuer.modelmerging.instance.pcm.surrogate.relation.DeploymentDeploymentRelation;
+import com.gstuer.modelmerging.instance.pcm.surrogate.relation.LinkResourceSpecificationRelation;
 import com.gstuer.modelmerging.test.utility.ElementFactory;
 
 public class DeploymentDeploymentRelationMergerTest extends RelationMergerTest<DeploymentDeploymentRelationMerger,
         PcmSurrogate, DeploymentDeploymentRelation, Deployment, Deployment> {
+    @Test
+    public void testRefineWithValidElementAddsCorrectImplications() {
+        // Test data
+        PcmSurrogate model = createEmptyModel();
+        DeploymentDeploymentRelationMerger merger = createMerger(model);
+        DeploymentDeploymentRelation relation = createUniqueReplaceable();
+
+        // Assertions: Pre-execution
+        assertTrue(merger.getImplications().isEmpty());
+
+        // Execution
+        merger.refine(relation);
+        Set<Replaceable> implications = merger.getImplications();
+
+        // Assertions: Post-execution
+        assertEquals(1, implications.size());
+        Replaceable implication = implications.stream().findFirst().orElseThrow();
+        assertEquals(LinkResourceSpecificationRelation.class, implication.getClass());
+        LinkResourceSpecificationRelation implicitRelation = (LinkResourceSpecificationRelation) implication;
+        assertEquals(relation, implicitRelation.getDestination());
+        assertTrue(implicitRelation.isPlaceholder());
+        assertTrue(implicitRelation.getSource().isPlaceholder());
+    }
+
     @Override
     protected DeploymentDeploymentRelation createRelation(Deployment source, Deployment destination,
             boolean isPlaceholder) {
