fb0af44fe74b3293f549b2a8bef33caea6ba3690
Moritz Gstuer <moritz@gstuer.com>
2022-07-12 17:34:36 +0200

    Add & test implicit depl. link on comp. assembly

--- bundles/tools.mdsd.mocore.instance/src/com/gstuer/modelmerging/instance/pcm/merger/relation/ComponentAssemblyRelationMerger.java
+++ bundles/tools.mdsd.mocore.instance/src/com/gstuer/modelmerging/instance/pcm/merger/relation/ComponentAssemblyRelationMerger.java
@@ -1,11 +1,48 @@
 package com.gstuer.modelmerging.instance.pcm.merger.relation;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.gstuer.modelmerging.framework.merger.RelationMerger;
 import com.gstuer.modelmerging.instance.pcm.surrogate.PcmSurrogate;
+import com.gstuer.modelmerging.instance.pcm.surrogate.element.Component;
+import com.gstuer.modelmerging.instance.pcm.surrogate.element.Deployment;
+import com.gstuer.modelmerging.instance.pcm.surrogate.relation.ComponentAllocationRelation;
 import com.gstuer.modelmerging.instance.pcm.surrogate.relation.ComponentAssemblyRelation;
+import com.gstuer.modelmerging.instance.pcm.surrogate.relation.DeploymentDeploymentRelation;
 
 public class ComponentAssemblyRelationMerger extends RelationMerger<PcmSurrogate, ComponentAssemblyRelation> {
     public ComponentAssemblyRelationMerger(PcmSurrogate model) {
         super(model, ComponentAssemblyRelation.class);
+    }
+
+    @Override
+    protected void refine(ComponentAssemblyRelation discovery) {
+        super.refine(discovery);
+
+        // Identify all allocations of the providing and consuming component in the assembly
+        Component provider = discovery.getSource().getSource();
+        Component consumer = discovery.getDestination().getSource();
+        List<ComponentAllocationRelation> allocations = this.getModel().getByType(ComponentAllocationRelation.class);
+        List<Deployment> providerAllocations = allocations.stream()
+                .filter(allocation -> allocation.getSource().equals(provider))
+                .map(ComponentAllocationRelation::getDestination)
+                .collect(Collectors.toList());
+        List<Deployment> consumerAllocations = allocations.stream()
+                .filter(allocation -> allocation.getSource().equals(consumer))
+                .map(ComponentAllocationRelation::getDestination)
+                .collect(Collectors.toList());
+
+        // Add link between allocation containers of assembled components if needed
+        for (Deployment providerContainer : providerAllocations) {
+            for (Deployment consumerContainer : consumerAllocations) {
+                if (!providerContainer.equals(consumerContainer)) {
+                    // Connect every providing container with each consuming one, except they are the same container
+                    DeploymentDeploymentRelation containerLink = new DeploymentDeploymentRelation(providerContainer,
+                            consumerContainer, true);
+                    this.addImplication(containerLink);
+                }
+            }
+        }
     }
 }
--- tests/tools.mdsd.mocore.test/src/com/gstuer/modelmerging/instance/pcm/merger/relation/ComponentAssemblyRelationMergerTest.java
+++ tests/tools.mdsd.mocore.test/src/com/gstuer/modelmerging/instance/pcm/merger/relation/ComponentAssemblyRelationMergerTest.java
@@ -1,16 +1,68 @@
 package com.gstuer.modelmerging.instance.pcm.merger.relation;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
 import com.gstuer.modelmerging.framework.merger.RelationMergerTest;
+import com.gstuer.modelmerging.framework.surrogate.Replaceable;
 import com.gstuer.modelmerging.instance.pcm.surrogate.PcmSurrogate;
 import com.gstuer.modelmerging.instance.pcm.surrogate.element.Component;
+import com.gstuer.modelmerging.instance.pcm.surrogate.element.Deployment;
 import com.gstuer.modelmerging.instance.pcm.surrogate.element.Interface;
+import com.gstuer.modelmerging.instance.pcm.surrogate.relation.ComponentAllocationRelation;
 import com.gstuer.modelmerging.instance.pcm.surrogate.relation.ComponentAssemblyRelation;
+import com.gstuer.modelmerging.instance.pcm.surrogate.relation.DeploymentDeploymentRelation;
 import com.gstuer.modelmerging.instance.pcm.surrogate.relation.InterfaceProvisionRelation;
 import com.gstuer.modelmerging.instance.pcm.surrogate.relation.InterfaceRequirementRelation;
 
 public class ComponentAssemblyRelationMergerTest extends RelationMergerTest<ComponentAssemblyRelationMerger,
         PcmSurrogate, ComponentAssemblyRelation, InterfaceProvisionRelation, InterfaceRequirementRelation> {
     private static final Interface RELATION_DESTINATION = Interface.getUniquePlaceholder();
+
+    @Test
+    public void testRefinementAddsImplicitDeploymentRelation() {
+        // Test data
+        PcmSurrogate model = createEmptyModel();
+        ComponentAssemblyRelationMerger merger = createMerger(model);
+
+        InterfaceProvisionRelation interfaceProvision = getUniqueNonPlaceholderSourceEntity();
+        InterfaceRequirementRelation interfaceRequirement = getUniqueNonPlaceholderDestinationEntity();
+        ComponentAssemblyRelation relation = createRelation(interfaceProvision, interfaceRequirement, false);
+
+        Deployment providingContainer = Deployment.getUniquePlaceholder();
+        Deployment requiringContainer = Deployment.getUniquePlaceholder();
+        ComponentAllocationRelation providingAllocation = new ComponentAllocationRelation(
+                interfaceProvision.getSource(), providingContainer, false);
+        ComponentAllocationRelation requiringAllocation = new ComponentAllocationRelation(
+                interfaceRequirement.getSource(), requiringContainer, false);
+
+        // Add containers & allocations to model
+        model.add(providingContainer);
+        model.add(requiringContainer);
+        model.add(providingAllocation);
+        model.add(requiringAllocation);
+
+        // Assertions: Pre-execution
+        assertTrue(merger.getImplications().isEmpty());
+
+        // Execution
+        merger.refine(relation);
+        Set<Replaceable> implications = new HashSet<>(merger.getImplications());
+
+        // Assertions: Post-execution
+        assertEquals(1, implications.size());
+        Replaceable implication = implications.stream().findFirst().orElseThrow();
+        assertEquals(DeploymentDeploymentRelation.class, implication.getClass());
+        DeploymentDeploymentRelation implicitDeploymentLink = (DeploymentDeploymentRelation) implication;
+        assertEquals(providingContainer, implicitDeploymentLink.getSource());
+        assertEquals(requiringContainer, implicitDeploymentLink.getDestination());
+        assertTrue(implicitDeploymentLink.isPlaceholder());
+    }
 
     @Override
     protected ComponentAssemblyRelation createRelation(InterfaceProvisionRelation source,
