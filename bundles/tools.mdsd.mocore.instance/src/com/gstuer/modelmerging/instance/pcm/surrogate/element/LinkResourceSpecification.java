477c66499937ec295a80d7dd111af450ec42436f
Moritz Gstuer <moritz@gstuer.com>
2022-07-12 13:03:44 +0200

    Add LinkResourceSpecification and its merger

---
+++ bundles/tools.mdsd.mocore.instance/src/com/gstuer/modelmerging/instance/pcm/surrogate/element/LinkResourceSpecification.java
@@ -0,0 +1,23 @@
+package com.gstuer.modelmerging.instance.pcm.surrogate.element;
+
+import org.palladiosimulator.pcm.resourceenvironment.CommunicationLinkResourceSpecification;
+import org.palladiosimulator.pcm.resourceenvironment.ResourceenvironmentFactory;
+
+import com.gstuer.modelmerging.framework.surrogate.Element;
+
+public class LinkResourceSpecification extends Element<CommunicationLinkResourceSpecification> {
+    public LinkResourceSpecification(CommunicationLinkResourceSpecification value, boolean isPlaceholder) {
+        super(value, isPlaceholder);
+    }
+
+    public static LinkResourceSpecification getUniquePlaceholder() {
+        String identifier = "Placeholder_" + getUniqueValue();
+        double failureProbability = 0D;
+
+        CommunicationLinkResourceSpecification value = ResourceenvironmentFactory.eINSTANCE
+                .createCommunicationLinkResourceSpecification();
+        value.setId(identifier);
+        value.setFailureProbability(failureProbability);
+        return new LinkResourceSpecification(value, true);
+    }
+}
