bcd8466857e1fb40494a0d534406c50bc7886da5
Moritz Gstuer <moritz@gstuer.com>
2022-07-12 13:10:09 +0200

    Add merger test classes for LinkResourceSpec.

---
+++ tests/tools.mdsd.mocore.test/src/com/gstuer/modelmerging/instance/pcm/merger/element/LinkResourceSpecificationMergerTest.java
@@ -0,0 +1,58 @@
+package com.gstuer.modelmerging.instance.pcm.merger.element;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+import com.gstuer.modelmerging.framework.merger.MergerTest;
+import com.gstuer.modelmerging.framework.surrogate.Replaceable;
+import com.gstuer.modelmerging.instance.pcm.surrogate.PcmSurrogate;
+import com.gstuer.modelmerging.instance.pcm.surrogate.element.LinkResourceSpecification;
+import com.gstuer.modelmerging.instance.pcm.surrogate.relation.LinkResourceSpecificationRelation;
+
+public class LinkResourceSpecificationMergerTest
+        extends MergerTest<LinkResourceSpecificationMerger, PcmSurrogate, LinkResourceSpecification> {
+    @Test
+    public void testRefineWithValidElementAddsCorrectImplications() {
+        // Test data
+        PcmSurrogate model = createEmptyModel();
+        LinkResourceSpecificationMerger merger = createMerger(model);
+        LinkResourceSpecification element = createUniqueReplaceable();
+
+        // Assertions: Pre-execution
+        assertTrue(merger.getImplications().isEmpty());
+
+        // Execution
+        merger.refine(element);
+        Set<Replaceable> implications = merger.getImplications();
+
+        // Assertions: Post-execution
+        assertEquals(1, implications.size());
+        Replaceable implication = implications.stream().findFirst().orElseThrow();
+        assertEquals(LinkResourceSpecificationRelation.class, implication.getClass());
+        LinkResourceSpecificationRelation relation = (LinkResourceSpecificationRelation) implication;
+        assertEquals(element, relation.getSource());
+        assertTrue(relation.isPlaceholder());
+        assertTrue(relation.getDestination().isPlaceholder());
+        assertTrue(relation.getDestination().getSource().isPlaceholder());
+        assertTrue(relation.getDestination().getDestination().isPlaceholder());
+    }
+
+    @Override
+    protected LinkResourceSpecificationMerger createMerger(PcmSurrogate model) {
+        return new LinkResourceSpecificationMerger(model);
+    }
+
+    @Override
+    protected PcmSurrogate createEmptyModel() {
+        return new PcmSurrogate();
+    }
+
+    @Override
+    protected LinkResourceSpecification createUniqueReplaceable() {
+        return LinkResourceSpecification.getUniquePlaceholder();
+    }
+}
