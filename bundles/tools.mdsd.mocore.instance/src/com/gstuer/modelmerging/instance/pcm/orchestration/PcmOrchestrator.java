cc6631a5cb12ed8adff3ea7ec075515fa1828db6
Moritz Gstuer <moritz@gstuer.com>
2022-07-13 20:49:28 +0200

    Add merger to pcm orchestrator constructor

--- bundles/tools.mdsd.mocore.instance/src/com/gstuer/modelmerging/instance/pcm/orchestration/PcmOrchestrator.java
+++ bundles/tools.mdsd.mocore.instance/src/com/gstuer/modelmerging/instance/pcm/orchestration/PcmOrchestrator.java
@@ -3,15 +3,26 @@ package com.gstuer.modelmerging.instance.pcm.orchestration;
 import com.gstuer.modelmerging.framework.orchestration.Orchestrator;
 import com.gstuer.modelmerging.instance.pcm.merger.element.ComponentMerger;
 import com.gstuer.modelmerging.instance.pcm.merger.element.DeploymentMerger;
+import com.gstuer.modelmerging.instance.pcm.merger.element.InterfaceMerger;
+import com.gstuer.modelmerging.instance.pcm.merger.element.LinkResourceSpecificationMerger;
+import com.gstuer.modelmerging.instance.pcm.merger.element.SignatureMerger;
 import com.gstuer.modelmerging.instance.pcm.merger.relation.ComponentAllocationRelationMerger;
 import com.gstuer.modelmerging.instance.pcm.merger.relation.ComponentAssemblyRelationMerger;
+import com.gstuer.modelmerging.instance.pcm.merger.relation.DeploymentDeploymentRelationMerger;
+import com.gstuer.modelmerging.instance.pcm.merger.relation.InterfaceProvisionRelationMerger;
+import com.gstuer.modelmerging.instance.pcm.merger.relation.InterfaceRequirementRelationMerger;
+import com.gstuer.modelmerging.instance.pcm.merger.relation.LinkResourceSpecificationRelationMerger;
+import com.gstuer.modelmerging.instance.pcm.merger.relation.SignatureProvisionRelationMerger;
 import com.gstuer.modelmerging.instance.pcm.surrogate.PcmSurrogate;
 
 public class PcmOrchestrator extends Orchestrator<PcmSurrogate> {
     public PcmOrchestrator(PcmSurrogate model) {
-        // TODO Add missing mergers to constructor
-        super(model, new ComponentMerger(model), new DeploymentMerger(model),
-                new ComponentAssemblyRelationMerger(model), new ComponentAllocationRelationMerger(model));
+        super(model, new SignatureMerger(model), new InterfaceMerger(model), new ComponentMerger(model),
+                new DeploymentMerger(model), new LinkResourceSpecificationMerger(model),
+                new SignatureProvisionRelationMerger(model), new InterfaceProvisionRelationMerger(model),
+                new InterfaceRequirementRelationMerger(model), new ComponentAssemblyRelationMerger(model),
+                new ComponentAllocationRelationMerger(model), new DeploymentDeploymentRelationMerger(model),
+                new LinkResourceSpecificationRelationMerger(model));
     }
 
     public PcmOrchestrator() {
