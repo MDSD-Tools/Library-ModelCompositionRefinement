72652f257313694763e3578af03fc32686009575
Moritz Gstuer <moritz@gstuer.com>
2022-07-17 17:27:27 +0200

    Add seff relation test class

---
+++ tests/tools.mdsd.mocore.test/src/com/gstuer/modelmerging/instance/pcm/surrogate/relation/ServiceEffectSpecificationRelationTest.java
@@ -0,0 +1,35 @@
+package com.gstuer.modelmerging.instance.pcm.surrogate.relation;
+
+import com.gstuer.modelmerging.framework.surrogate.RelationTest;
+import com.gstuer.modelmerging.instance.pcm.surrogate.element.Component;
+import com.gstuer.modelmerging.instance.pcm.surrogate.element.Interface;
+import com.gstuer.modelmerging.instance.pcm.surrogate.element.ServiceEffectSpecification;
+import com.gstuer.modelmerging.instance.pcm.surrogate.element.Signature;
+import com.gstuer.modelmerging.instance.pcm.surrogate.relation.pseudo.ComponentSignatureProvisionRelation;
+
+public class ServiceEffectSpecificationRelationTest extends RelationTest<ServiceEffectSpecificationRelation,
+        ComponentSignatureProvisionRelation, ServiceEffectSpecification> {
+    @Override
+    protected ServiceEffectSpecificationRelation createRelation(ComponentSignatureProvisionRelation source,
+            ServiceEffectSpecification destination, boolean isPlaceholder) {
+        return new ServiceEffectSpecificationRelation(source, destination, isPlaceholder);
+    }
+
+    @Override
+    protected ComponentSignatureProvisionRelation getUniqueSourceEntity() {
+        Component component = Component.getUniquePlaceholder();
+        Interface interfsc = Interface.getUniquePlaceholder();
+        Signature signature = Signature.getUniquePlaceholder();
+        InterfaceProvisionRelation interfaceProvision = new InterfaceProvisionRelation(component,
+                interfsc, true);
+        SignatureProvisionRelation signatureProvision = new SignatureProvisionRelation(signature,
+                interfsc, true);
+        return new ComponentSignatureProvisionRelation(interfaceProvision, signatureProvision, false);
+    }
+
+    @Override
+    protected ServiceEffectSpecification getUniqueDestinationEntity() {
+        return ServiceEffectSpecification.getUniquePlaceholder();
+    }
+
+}
