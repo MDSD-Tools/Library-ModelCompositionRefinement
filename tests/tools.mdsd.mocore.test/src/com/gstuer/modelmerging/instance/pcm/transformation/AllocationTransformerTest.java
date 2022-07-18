a199ac64d9c71ecdc44bccca68d3abd76b26416b
Moritz Gstuer <moritz@gstuer.com>
2022-07-18 19:56:34 +0200

    Add draft of allocation transformer test

--- tests/tools.mdsd.mocore.test/src/com/gstuer/modelmerging/instance/pcm/transformation/AllocationTransformerTest.java
+++ tests/tools.mdsd.mocore.test/src/com/gstuer/modelmerging/instance/pcm/transformation/AllocationTransformerTest.java
@@ -1,12 +1,40 @@
 package com.gstuer.modelmerging.instance.pcm.transformation;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.palladiosimulator.pcm.allocation.Allocation;
 
 import com.gstuer.modelmerging.framework.transformation.TransformerTest;
 import com.gstuer.modelmerging.instance.pcm.surrogate.PcmSurrogate;
+import com.gstuer.modelmerging.instance.pcm.surrogate.element.Component;
+import com.gstuer.modelmerging.instance.pcm.surrogate.element.Deployment;
+import com.gstuer.modelmerging.instance.pcm.surrogate.relation.ComponentAllocationRelation;
 
 public class AllocationTransformerTest extends TransformerTest<AllocationTransformer, PcmSurrogate, Allocation> {
-    // TODO Implement Test Methods
+    @ParameterizedTest
+    @ValueSource(booleans = { true, false })
+    public void testTransformSingleAllocation(boolean isPlaceholderAllocation) {
+        // Test data
+        AllocationTransformer transformer = createTransformer();
+        PcmSurrogate model = createEmptyModel();
+        Component component = Component.getUniquePlaceholder();
+        Deployment deployment = Deployment.getUniquePlaceholder();
+        ComponentAllocationRelation allocationRelation = new ComponentAllocationRelation(component,
+                deployment, isPlaceholderAllocation);
+
+        model.add(component);
+        model.add(deployment);
+        model.add(allocationRelation);
+
+        // Execution
+        Allocation allocation = transformer.transform(model);
+
+        // Assertion
+        assertNotNull(allocation);
+        // TODO Check whether all elements where transformed correctly
+    }
 
     @Override
     protected AllocationTransformer createTransformer() {
