790d4fa5291e5baa12fb669285332f5c18c6bc11
Moritz Gstuer <moritz@gstuer.com>
2022-08-16 14:45:10 +0200

    Add junit test class for CoReX case study

---
+++ tests/tools.mdsd.mocore.test/src/com/gstuer/modelmerging/evaluation/CoReXTest.java
@@ -0,0 +1,27 @@
+package com.gstuer.modelmerging.evaluation;
+
+import java.io.File;
+
+public class CoReXTest extends CaseStudyTest {
+    private static final String FILE_PATH_PREFIX = "./resources/evaluation/case-studies/CoReX/";
+
+    @Override
+    protected File getRepositoryFile() {
+        return new File(FILE_PATH_PREFIX + "minimalProject.repository");
+    }
+
+    @Override
+    protected File getSystemFile() {
+        return new File(FILE_PATH_PREFIX + "minimalProject.system");
+    }
+
+    @Override
+    protected File getAllocationFile() {
+        return new File(FILE_PATH_PREFIX + "minimalProject.allocation");
+    }
+
+    @Override
+    protected File getResourceEnvironmentFile() {
+        return new File(FILE_PATH_PREFIX + "minimalProject.resourceenvironment");
+    }
+}
