c1dc64e1a128f1fbc1ee1c612177a4c820870156
Moritz Gstuer <moritz@gstuer.com>
2022-08-27 14:58:02 +0200

    Add parameters for refinement evaluation of CoReX

--- tests/tools.mdsd.mocore.test/src/com/gstuer/modelmerging/evaluation/CoReXTest.java
+++ tests/tools.mdsd.mocore.test/src/com/gstuer/modelmerging/evaluation/CoReXTest.java
@@ -2,8 +2,43 @@ package com.gstuer.modelmerging.evaluation;
 
 import java.io.File;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class CoReXTest extends CaseStudyTest {
     private static final String FILE_PATH_PREFIX = "./resources/evaluation/case-studies/CoReX/";
+
+    // region Refinement Evaluation
+
+    @Override
+    @ParameterizedTest
+    @ValueSource(strings = { "_OdXCIByyEe2EUeCzYK37WA", "_OHlmcByyEe2EUeCzYK37WA" })
+    public void testRefinementComponentWithoutContainer(String componentIdentifier) {
+        super.testRefinementComponentWithoutContainer(componentIdentifier);
+    }
+
+    @Override
+    @ParameterizedTest
+    @ValueSource(strings = { "_Qn_GoByyEe2EUeCzYK37WA" })
+    public void testRefinementInterfaceWithoutComponent(String interfaceIdentifier) {
+        super.testRefinementInterfaceWithoutComponent(interfaceIdentifier);
+    }
+
+    @Override
+    @ParameterizedTest
+    @ValueSource(strings = { "_Di9fMBy0Ee2EUeCzYK37WA" })
+    public void testRefinementComponentAssemblyRequiresContainerLink(String linkIdentifier) {
+        super.testRefinementComponentAssemblyRequiresContainerLink(linkIdentifier);
+    }
+
+    @Override
+    @ParameterizedTest
+    @ValueSource(strings = { "_Di9fMBy0Ee2EUeCzYK37WA" })
+    public void testRefinementContainerLinkNotReplacedIndirectly(String linkIdentifier) {
+        super.testRefinementContainerLinkNotReplacedIndirectly(linkIdentifier);
+    }
+
+    // endregion
 
     @Override
     protected String getCaseStudyName() {
