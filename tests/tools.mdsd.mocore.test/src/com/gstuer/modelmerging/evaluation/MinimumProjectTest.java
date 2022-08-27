2dbea755e2edfeaef8c65c69cefc0ec40d7b877a
Moritz Gstuer <moritz@gstuer.com>
2022-08-27 16:11:09 +0200

    Add ref. parameters for mediastore & min. project

--- tests/tools.mdsd.mocore.test/src/com/gstuer/modelmerging/evaluation/MinimumProjectTest.java
+++ tests/tools.mdsd.mocore.test/src/com/gstuer/modelmerging/evaluation/MinimumProjectTest.java
@@ -2,9 +2,39 @@ package com.gstuer.modelmerging.evaluation;
 
 import java.io.File;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class MinimumProjectTest extends CaseStudyTest {
     private static final String FILE_PATH_PREFIX = "./resources/evaluation/case-studies/palladio-example-models/"
             + "Minimum_Project_Example/";
+
+    @Override
+    @ParameterizedTest
+    @ValueSource(strings = { "_os1t4CHbEd62GabW1zGSBw" })
+    public void testRefinementComponentWithoutContainer(String componentIdentifier) {
+        super.testRefinementComponentWithoutContainer(componentIdentifier);
+    }
+
+    @Override
+    @ParameterizedTest
+    @ValueSource(strings = { "_n7g-oCHbEd62GabW1zGSBw" })
+    public void testRefinementInterfaceWithoutComponent(String interfaceIdentifier) {
+        super.testRefinementInterfaceWithoutComponent(interfaceIdentifier);
+    }
+
+    @Override
+    @Disabled("The case study does not provide a container link.")
+    public void testRefinementComponentAssemblyRequiresContainerLink(String linkIdentifier) {
+        super.testRefinementComponentAssemblyRequiresContainerLink(linkIdentifier);
+    }
+
+    @Override
+    @Disabled("The case study does not provide a container link.")
+    public void testRefinementContainerLinkNotReplacedIndirectly(String linkIdentifier) {
+        super.testRefinementContainerLinkNotReplacedIndirectly(linkIdentifier);
+    }
 
     @Override
     protected String getCaseStudyName() {
