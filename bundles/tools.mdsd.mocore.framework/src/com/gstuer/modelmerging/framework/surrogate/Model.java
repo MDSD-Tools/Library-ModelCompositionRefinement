6de543fabf8fd3c29ae51777fbcf06aa50d9cb4e
Moritz Gstuer <moritz@gstuer.com>
2022-11-06 18:01:42 +0100

    Move PCM-indep. model implementation to framework

--- bundles/tools.mdsd.mocore.framework/src/com/gstuer/modelmerging/framework/surrogate/Model.java
+++ bundles/tools.mdsd.mocore.framework/src/com/gstuer/modelmerging/framework/surrogate/Model.java
@@ -1,14 +1,28 @@
 package com.gstuer.modelmerging.framework.surrogate;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
-public interface Model {
-    <T extends Replaceable> List<T> getByType(Class<T> type);
+public class Model {
+    private final TypedDistinctMultiMap<Replaceable> replaceables;
 
-    void add(Replaceable replaceable);
+    public Model() {
+        this.replaceables = new TypedDistinctMultiMap<>();
+    }
 
-    boolean contains(Replaceable replaceable);
+    public <T extends Replaceable> List<T> getByType(Class<T> type) {
+        return this.replaceables.get(type);
+    }
+
+    public void add(Replaceable replaceable) {
+        this.replaceables.put(replaceable);
+    }
+
+    public boolean contains(Replaceable replaceable) {
+        return this.replaceables.containsElement(replaceable);
+    }
 
     /**
      * Executes a replacement of a certain {@link Replaceable replaceable} in the model. For this purpose, all affected
@@ -21,5 +35,23 @@ public interface Model {
      * @return The successors of the affected and removed replaceables in the model including the original's
      *         replacement.
      */
-    Set<Replaceable> replace(Replaceable original, Replaceable replacement);
+    public Set<Replaceable> replace(Replaceable original, Replaceable replacement) {
+        if (Objects.isNull(original) || Objects.isNull(replacement)) {
+            throw new NullPointerException();
+        }
+        Set<Replaceable> implications = new HashSet<>();
+        for (Replaceable predecessor : this.replaceables) {
+            if (predecessor.canReplace(original)) {
+                /*
+                 * If a predecessor needs replacement, create successor of obsolete replaceable. Then delete obsolete
+                 * replaceable from model and return successor as implication for further merging. The model cannot add
+                 * the successor itself because it might have consequences for other replaceables in the model.
+                 */
+                Replaceable successor = predecessor.replace(original, replacement);
+                this.replaceables.remove(predecessor);
+                implications.add(successor);
+            }
+        }
+        return implications;
+    }
 }
--- bundles/tools.mdsd.mocore.instance/src/com/gstuer/modelmerging/instance/pcm/surrogate/PcmSurrogate.java
+++ bundles/tools.mdsd.mocore.instance/src/com/gstuer/modelmerging/instance/pcm/surrogate/PcmSurrogate.java
@@ -1,54 +1,9 @@
 package com.gstuer.modelmerging.instance.pcm.surrogate;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
-
 import com.gstuer.modelmerging.framework.surrogate.Model;
-import com.gstuer.modelmerging.framework.surrogate.Replaceable;
-import com.gstuer.modelmerging.framework.surrogate.TypedDistinctMultiMap;
 
-public class PcmSurrogate implements Model {
-    private final TypedDistinctMultiMap<Replaceable> replaceables;
-
+public class PcmSurrogate extends Model {
     public PcmSurrogate() {
-        this.replaceables = new TypedDistinctMultiMap<>();
-    }
-
-    @Override
-    public <T extends Replaceable> List<T> getByType(Class<T> type) {
-        return this.replaceables.get(type);
-    }
-
-    @Override
-    public void add(Replaceable replaceable) {
-        this.replaceables.put(replaceable);
-    }
-
-    @Override
-    public boolean contains(Replaceable replaceable) {
-        return this.replaceables.containsElement(replaceable);
-    }
-
-    @Override
-    public Set<Replaceable> replace(Replaceable original, Replaceable replacement) {
-        if (Objects.isNull(original) || Objects.isNull(replacement)) {
-            throw new NullPointerException();
-        }
-        Set<Replaceable> implications = new HashSet<>();
-        for (Replaceable predecessor : this.replaceables) {
-            if (predecessor.canReplace(original)) {
-                /*
-                 * If a predecessor needs replacement, create successor of obsolete replaceable. Then delete obsolete
-                 * replaceable from model and return successor as implication for further merging. The model cannot add
-                 * the successor itself because it might have consequences for other replaceables in the model.
-                 */
-                Replaceable successor = predecessor.replace(original, replacement);
-                this.replaceables.remove(predecessor);
-                implications.add(successor);
-            }
-        }
-        return implications;
+        super();
     }
 }
