a78f9a945914ac54df13d0359e5a347cdd4b9e18
Moritz Gstuer <moritz@gstuer.com>
2022-12-18 12:51:09 +0100

    Remove instance & test from framework repository

--- bundles/tools.mdsd.mocore.instance/src/com/gstuer/modelmerging/instance/pcm/surrogate/PcmSurrogate.java
+++
@@ -1,9 +0,0 @@
-package com.gstuer.modelmerging.instance.pcm.surrogate;
-
-import com.gstuer.modelmerging.framework.surrogate.Model;
-
-public class PcmSurrogate extends Model {
-    public PcmSurrogate() {
-        super();
-    }
-}
