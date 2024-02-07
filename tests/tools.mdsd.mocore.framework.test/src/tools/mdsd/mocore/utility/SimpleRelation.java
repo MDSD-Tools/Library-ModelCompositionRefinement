5cc3bbf0534786db2efb286172c5e9776ab07569
dr6817 <dr6817@kit.edu>
2024-02-07 10:23:57 +0100

    Clean Up

--- tests/tools.mdsd.mocore.framework.test/src/tools/mdsd/mocore/utility/SimpleRelation.java
+++ tests/tools.mdsd.mocore.framework.test/src/tools/mdsd/mocore/utility/SimpleRelation.java
@@ -1,3 +1,11 @@
+/* Copyright (C) 2023 Moritz Gstuer - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the EUPL 1.2 license.
+ *
+ * You should have received a copy of the EUPL 1.2 license
+ * with this file. If not, please visit:
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ */
 package tools.mdsd.mocore.utility;
 
 import tools.mdsd.mocore.framework.surrogate.Relation;
